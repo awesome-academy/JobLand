649191201fbe9922636fe13320969c75d6fb1886
Nguyen Dinh Son <son261197@gmail.com>
2020-07-21 11:31:02 +0700

    Basic Profile

---
+++ app/controllers/employer_controller.rb
@@ -0,0 +1,19 @@
+class EmployerController < ApplicationController
+  def home
+end
+
+def help
+   end
+
+
+  def about
+  end
+
+  def products
+  end
+
+end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'users/new'
-  get '/res-post', to: 'static_pages#res-post'
-  get '/job-post', to: 'static_pages#job-post'
-  get '/new-post', to: 'static_pages#new-post'
+  get '/job-post', to: 'employer#job-post'
+  get '/new-post', to: 'employer#new-post'
+  get '/create-cv', to: 'employer#create-cv'
   get '/home', to: 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/all', to: 'static_pages#products'
