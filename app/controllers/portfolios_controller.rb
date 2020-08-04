cff5d1ee95bdcd203da2c0db8356ab35cade8df8
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-08-04 16:51:57 +0700

    portfolio complete

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,17 +1,50 @@
 class PortfoliosController < ApplicationController
 	before_action :authenticate_user!
+
+  def index
+    @portfolio = Portfolio.new
+    @portfolios = current_user.cv.portfolios
+  end
   
 	def new
     @portfolio = Portfolio.new
+    respond_to do |format|
+      format.html {render(partial: "portfolio_form")}
+      format.js
+      format.json
+    end
 	end
   
   def create
   	@portfolio = current_user.cv.portfolios.build portfolio_params
   	@portfolio.image.attach params[:portfolio][:image]
   	@portfolio.save
-    flash[:success] = "Created"
+    @portfolios = current_user.cv.portfolios
+    @portfolio = Portfolio.new
     respond_to do |format|
-      format.html {render(partial: "portfolio")}
+      format.html {render(partial: "portfolio_section")}
+      format.js
+      format.json
+    end
+  end
+
+  def edit
+    @portfolio = Portfolio.find params[:id]
+    respond_to do |format|
+      format.html {render(partial: "portfolio_form")}
+      format.js
+      format.json
+    end
+  end
+
+  def update
+    @portfolio = Portfolio.find params[:id]
+    @portfolio.image.attach params[:portfolio][:image]
+    @portfolio.update(portfolio_params)
+    @portfolio = Portfolio.new
+    @portfolios = current_user.cv.portfolios
+    respond_to do |format|
+      format.html {render(partial: "portfolio_section")}
       format.js
       format.json
     end
@@ -20,7 +53,6 @@ class PortfoliosController < ApplicationController
   def destroy
   	@portfolio = Portfolio.find params[:id]
   	@portfolio.destroy
-  	flash[:success] = "Deleted"
     respond_to do |format|
       format.html
       format.js
