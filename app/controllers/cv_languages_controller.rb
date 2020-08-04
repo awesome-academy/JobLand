f8c4de60f11054ea0d944ff02821cad862598b88
Nguyen Dinh Son <son261197@gmail.com>
2020-08-04 20:48:29 +0700

    Ajax Language-Experience

--- app/controllers/cv_languages_controller.rb
+++ app/controllers/cv_languages_controller.rb
@@ -1,14 +1,22 @@
 class CvLanguagesController < ApplicationController
+  def index
+     @cv_language  =  CvLanguage.new
+  end
+
   def new
     @cv_language  =  CvLanguage.new
+    respond_to do |format|
+      format.html {render(partial: "cv_language_form")}
+      format.js
+      format.json
+    end
   end
 
   def create
     @cv_language = current_user.cv.cv_languages.build language_params
     @cv_language.save
-    flash[:success] = t("global.experience.success")
     respond_to do |format|
-      format.html {render(partial: "cv_language")}
+      format.html {render(partial: "cv_language_section")}
       format.js
       format.json
     end
@@ -17,7 +25,6 @@ class CvLanguagesController < ApplicationController
   def destroy
     @cv_language = CvLanguage.find params[:id]
     @cv_language.destroy
-    flash[:success] = t("global.experience.remove")
     respond_to do |format|
       format.html
       format.js
@@ -29,3 +36,5 @@ class CvLanguagesController < ApplicationController
     params.require(:cv_language).permit :language_id
   end
 end
+
+
