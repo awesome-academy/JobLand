a18ead856586bd34154e3c95aead3ff2f5e76af5
HoangTuan <tuan.hm.bn@gmail.com>
2020-08-07 11:05:30 +0700

    education done

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,5 @@
 class Experience < ApplicationRecord
   belongs_to :cv
   validates :title, presence: true, length: { maximum: 140 }
-  validates :description, presence: true, length: { maximum: 140 }
+  validates :description, presence: true, length: { maximum: 500 }
 end
